431d9d88ea7993965931b8ad03eddbfe43c918df
RustyNail-27 <russell.d.g.walton@gmail.com>
2016-04-01 17:00:01 -0700

    determines monthly student expenses based on student input
    C# program that determines monthly student expenses based on student
    input

---
+++ Seminar 5 Problem 2/Seminar 5 Problem 2/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Seminar_5_Problem_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentLabel = new System.Windows.Forms.Label();
+            this.dayLabel = new System.Windows.Forms.Label();
+            this.studentListBox = new System.Windows.Forms.ListBox();
+            this.dayListBox = new System.Windows.Forms.ListBox();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.allInfoButton = new System.Windows.Forms.Button();
+            this.dayInfoButton = new System.Windows.Forms.Button();
+            this.studentInfoButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // studentLabel
+            // 
+            this.studentLabel.AutoSize = true;
+            this.studentLabel.Location = new System.Drawing.Point(9, 9);
+            this.studentLabel.Name = "studentLabel";
+            this.studentLabel.Size = new System.Drawing.Size(44, 13);
+            this.studentLabel.TabIndex = 0;
+            this.studentLabel.Text = "Student";
+            // 
+            // dayLabel
+            // 
+            this.dayLabel.AutoSize = true;
+            this.dayLabel.Location = new System.Drawing.Point(118, 9);
+            this.dayLabel.Name = "dayLabel";
+            this.dayLabel.Size = new System.Drawing.Size(26, 13);
+            this.dayLabel.TabIndex = 1;
+            this.dayLabel.Text = "Day";
+            // 
+            // studentListBox
+            // 
+            this.studentListBox.FormattingEnabled = true;
+            this.studentListBox.Location = new System.Drawing.Point(12, 28);
+            this.studentListBox.Name = "studentListBox";
+            this.studentListBox.Size = new System.Drawing.Size(92, 56);
+            this.studentListBox.TabIndex = 2;
+            // 
+            // dayListBox
+            // 
+            this.dayListBox.FormattingEnabled = true;
+            this.dayListBox.Location = new System.Drawing.Point(121, 28);
+            this.dayListBox.Name = "dayListBox";
+            this.dayListBox.Size = new System.Drawing.Size(88, 56);
+            this.dayListBox.TabIndex = 3;
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.Location = new System.Drawing.Point(12, 114);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(491, 108);
+            this.outputListBox.TabIndex = 4;
+            // 
+            // allInfoButton
+            // 
+            this.allInfoButton.Location = new System.Drawing.Point(214, 239);
+            this.allInfoButton.Name = "allInfoButton";
+            this.allInfoButton.Size = new System.Drawing.Size(75, 61);
+            this.allInfoButton.TabIndex = 5;
+            this.allInfoButton.Text = "Display All Data";
+            this.allInfoButton.UseVisualStyleBackColor = true;
+            this.allInfoButton.Click += new System.EventHandler(this.allInfoButton_Click);
+            // 
+            // dayInfoButton
+            // 
+            this.dayInfoButton.Location = new System.Drawing.Point(111, 239);
+            this.dayInfoButton.Name = "dayInfoButton";
+            this.dayInfoButton.Size = new System.Drawing.Size(75, 61);
+            this.dayInfoButton.TabIndex = 6;
+            this.dayInfoButton.Text = "Display Day Data";
+            this.dayInfoButton.UseVisualStyleBackColor = true;
+            this.dayInfoButton.Click += new System.EventHandler(this.dayInfoButton_Click);
+            // 
+            // studentInfoButton
+            // 
+            this.studentInfoButton.Location = new System.Drawing.Point(12, 239);
+            this.studentInfoButton.Name = "studentInfoButton";
+            this.studentInfoButton.Size = new System.Drawing.Size(75, 61);
+            this.studentInfoButton.TabIndex = 7;
+            this.studentInfoButton.Text = "Display Student Data";
+            this.studentInfoButton.UseVisualStyleBackColor = true;
+            this.studentInfoButton.Click += new System.EventHandler(this.studentInfoButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(12, 98);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(65, 13);
+            this.outputLabel.TabIndex = 8;
+            this.outputLabel.Text = "Output Data";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(428, 239);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 61);
+            this.exitButton.TabIndex = 9;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(320, 239);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 61);
+            this.clearButton.TabIndex = 10;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 312);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.studentInfoButton);
+            this.Controls.Add(this.dayInfoButton);
+            this.Controls.Add(this.allInfoButton);
+            this.Controls.Add(this.outputListBox);
+            this.Controls.Add(this.dayListBox);
+            this.Controls.Add(this.studentListBox);
+            this.Controls.Add(this.dayLabel);
+            this.Controls.Add(this.studentLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label studentLabel;
+        private System.Windows.Forms.Label dayLabel;
+        private System.Windows.Forms.ListBox studentListBox;
+        private System.Windows.Forms.ListBox dayListBox;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Button allInfoButton;
+        private System.Windows.Forms.Button dayInfoButton;
+        private System.Windows.Forms.Button studentInfoButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Seminar 5 Problem 2/Seminar 5 Problem 2/Form1.cs
@@ -0,0 +1,272 @@
+﻿/****    
+*
+* Name: Russell Walton
+* Student Number: T00249622
+* Seminar Number: 5
+* Due Date:  Mar.3, 2015
+*
+* Program Purpose: Application reads data from a text file and populates arrays.
+ * Then allows user to display student name and expense info, or show daily expense info,
+ * or display all student names and all daily expense info and averages.
+*
+****/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Seminar_5_Problem_2
+{
+    public partial class Form1 : Form
+    {
+        string[] names;
+        int[,] dailyExp;
+        double[] studentAvrg;
+        double[] dayAvrg;
+        StreamReader inputFile;
+        string str;
+        string dimensions;
+        int nameSize, daySize;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                inputFile = File.OpenText(openFile.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Operation cancelled.");
+            }
+
+            while (!inputFile.EndOfStream)
+            {
+                dimensions = inputFile.ReadLine();
+ 
+                string[] dTokens = dimensions.Split(null);
+                
+                nameSize = int.Parse(dTokens[0]);
+                //read the number of days from file
+                daySize = int.Parse(dTokens[1]);
+
+                names = new string[nameSize];
+                studentAvrg = new double[nameSize];
+                dayAvrg = new double[daySize];
+                dailyExp = new int[nameSize,daySize];
+                
+                //reads the student names and stores them into names array
+                for(int i = 0; i < nameSize; i++)
+                {
+                    names[i] = inputFile.ReadLine();
+                }
+
+                //store values from file into 2D dailyExp array
+                for(int i = 0; i < nameSize; i++)
+                {
+                    str = inputFile.ReadLine();
+
+                    string[] tokens = str.Split(null);
+
+                    for(int j = 0; j < daySize; j++)
+                    {
+                        dailyExp[i,j] = int.Parse(tokens[j]);
+                    }
+                }
+
+                //calculate student average, store in studentAvrg array
+                for (int i = 0; i < nameSize; i++)
+                {
+                    double stTotal = 0;
+                    double stAvrg = 0;
+
+                    for (int j = 0; j < daySize; j++)
+                    {
+                        stTotal += dailyExp[i,j];
+                    }
+
+                    stAvrg = stTotal / daySize;
+
+                    studentAvrg[i] = stAvrg;
+                }
+                
+                //calculate day average, store in dayAvrg array
+                for (int j = 0; j < daySize; j++)
+                {
+                    double dTotal = 0;
+                    double dAvrg = 0;
+
+                    for (int i = 0; i < nameSize; i++)
+                    {
+                        dTotal += dailyExp[i, j];
+                    }
+
+                    dAvrg = dTotal / nameSize;
+
+                    dayAvrg[j] = dAvrg;
+                }
+
+                //initialize studentListBox
+                for(int i = 0; i < nameSize; i++)
+                {
+                    studentListBox.Items.Add("Student " + (i+1));
+                }
+
+                //initialize dayListBox
+                for (int i = 0; i < daySize; i++)
+                {
+                    dayListBox.Items.Add("Day " + (i + 1));
+                }
+
+            }
+        }
+
+        private void studentInfoButton_Click(object sender, EventArgs e)
+        {
+            string line;
+
+            line = "Student Name         Daily Spending";
+
+            outputListBox.Items.Add(line);
+
+            line = "";
+
+            line = "                                ";
+
+            for (int i = 0; i < daySize; i++)
+            {
+                line += "Day "+(i+1)+ "  ";               
+            }
+
+            line += "Daily Average";
+
+            outputListBox.Items.Add(line);
+
+            line = "";
+            
+            line = names[studentListBox.SelectedIndex];
+
+            line += "                 ";
+
+            for (int i = 0; i < daySize; i++)
+            {
+                line += dailyExp[studentListBox.SelectedIndex, i]+"        ";
+            }
+
+            line += studentAvrg[studentListBox.SelectedIndex].ToString("0.##");
+
+            outputListBox.Items.Add(line);
+        }
+
+        private void dayInfoButton_Click(object sender, EventArgs e)
+        {
+            string line;
+
+            line = "Student Name         Daily Spending";
+
+            outputListBox.Items.Add(line);
+
+            line = "";
+
+            line = "                                ";
+
+            line += "Day " +(dayListBox.SelectedIndex+1)+ "  ";
+
+            outputListBox.Items.Add(line);
+
+            for (int i = 0; i < nameSize; i++)
+            {
+                line = "";
+
+                line += names[i];
+
+                line += "                ";
+
+                line += dailyExp[i, dayListBox.SelectedIndex];
+
+                outputListBox.Items.Add(line);
+            }
+
+            line = "Average per day      " + dayAvrg[dayListBox.SelectedIndex].ToString("0.##");
+
+            outputListBox.Items.Add(line);    
+        }
+
+        private void allInfoButton_Click(object sender, EventArgs e)
+        {
+            string line;
+            double total = 0.0;
+
+            line = "Student Name         Daily Spending";
+
+            outputListBox.Items.Add(line);
+
+            line = "";
+
+            line = "                                ";
+
+            for (int i = 0; i < daySize; i++)
+            {
+                line += "Day "+(i+1)+ "     ";               
+            }
+
+            line += "Daily Average";
+
+            outputListBox.Items.Add(line);
+            
+            for(int j = 0; j < nameSize; j++)
+            {
+                line = names[j];
+                total += dayAvrg[j];
+
+                line += "                 ";
+
+                for (int i = 0; i < daySize; i++)
+                {
+                    line += dailyExp[j, i]+"           ";
+                }
+
+                line += studentAvrg[j].ToString("0.##");
+
+                outputListBox.Items.Add(line);                
+            }
+            line = "Average per day      ";
+
+            for (int i = 0; i < daySize; i++)
+            {
+                line += dayAvrg[i].ToString("0.##") + "        ";
+            }
+
+            double personAvrg = total / (double)nameSize;
+
+            line += personAvrg.ToString("0.##");
+
+            outputListBox.Items.Add(line); 
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            outputListBox.Items.Clear();
+        }
+        
+
+
+    }
+}
